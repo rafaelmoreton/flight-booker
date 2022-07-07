bed16b357cb989cfc2f254dcf94a40c4342d5e3e
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-07-07 17:26:50 -0300

    Associate flights and passengers through bookings

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,5 @@ class Flight < ApplicationRecord
   belongs_to :arrival, class_name: "Airport"
 
   has_many :bookings
+  has_many :passengers, through: :bookings
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,4 @@
 class Passenger < ApplicationRecord
   belongs_to :booking
+  delegate :flight, to: :booking
 end
