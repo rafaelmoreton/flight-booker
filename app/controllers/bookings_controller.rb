3b531ac913b9434fa15af01d9e793af0985040cc
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-07-07 16:58:11 -0300

    Show booking form with fields for each passenger

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,10 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new(flight_id: params[:flight_id])
+    params[:passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'flights/new'
   get 'flights/', to: "flights#index"
   get 'bookings/new', to: "bookings#new"
+  get 'bookings', to: "bookings#index", as: :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
