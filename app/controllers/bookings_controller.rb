c0448aadaa41d24ea2e99c65e3ce518e39556e49
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-07-11 09:12:01 -0300

    Create new passengers with new bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new(flight_id: params[:flight_id])
+    cookies[:flight_id] = params[:flight_id]
+    @booking = Booking.new(flight_id: cookies[:flight_id])
     params[:passengers].to_i.times { @booking.passengers.build }
   end
 
   def create
-    
+    @booking = Booking.new(booking_params)
+    @booking.flight_id = cookies[:flight_id]
+
+    if @booking.save
+      redirect_to booking_path(@booking), notice: "Booking confirmed"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(passengers_attributes: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'flights/new'
   get 'flights/', to: "flights#index"
   get 'bookings/new', to: "bookings#new"
-  get 'bookings', to: "bookings#index", as: :bookings
+  post 'bookings/', to: "bookings#create", as: :booking_new
+  get 'bookings/', to: "bookings#index", as: :bookings
+  get 'bookings/:id', to: "bookings#show", as: :booking
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
